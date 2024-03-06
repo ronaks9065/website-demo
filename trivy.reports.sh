1c4ab2e3e32fc3ddad7f28dd0d56216141b3f4bd
ronaks9065 <115705180+ronaks9065@users.noreply.github.com>
2024-03-06 10:50:43 +0530

    Update trivy.reports.sh

--- trivy.reports.sh
+++ trivy.reports.sh
@@ -31,6 +31,8 @@ else
 fi
 trivy --severity LOW,MEDIUM,HIGH ronak1907/webapp:latest > "$report_filename"
 aws s3 cp "$report_filename" s3://trivy-scan-bucket/"$report_filename"
-# aws sns publish --topic-arn "arn:aws:sns:ap-south-1:149815208654:trivy_scan_mail" --subject "Trivy Report" --message "Trivy report is available at" echo "<a href='view-source:https://trivy-scan-bucket.s3.ap-south-1.amazonaws.com/$report_filename'>View Trivy Scan Report</a>"
-message="<html><body>Trivy report is available at <a href='data:text/plain,https://trivy-scan-bucket.s3.ap-south-1.amazonaws.com/$report_filename'>View Trivy Scan Report</a></body></html>"
-aws sns publish --topic-arn "arn:aws:sns:ap-south-1:149815208654:trivy_scan_mail" --subject "Trivy Report" --message "$message"
+# aws sns publish --topic-arn "arn:aws:sns:ap-south-1:149815208654:trivy_scan_mail" --subject "Trivy Report" --message "Trivy report is available at view-source:https://trivy-scan-bucket.s3.ap-south-1.amazonaws.com/$report_filename View Trivy Scan Report."
+report_link="https://trivy-scan-bucket.s3.ap-south-1.amazonaws.com/$report_filename"
+message="Trivy report is available at <a href=\"$report_link\">$report_link</a>. View Trivy Scan Report."
+
+aws sns publish --topic-arn "arn:aws:sns:ap-south-1:149815208654:trivy_scan_mail" --subject "Trivy Report" --message "$message" --message-structure "string"
