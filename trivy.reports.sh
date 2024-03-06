c8962d638cef9990d177b6f11240399385fcfc7e
ronaks9065 <115705180+ronaks9065@users.noreply.github.com>
2024-03-06 11:13:51 +0530

    Update trivy.reports.sh

--- trivy.reports.sh
+++ trivy.reports.sh
@@ -34,4 +34,4 @@ aws s3 cp "$report_filename" s3://trivy-scan-bucket/"$report_filename"
 # aws sns publish --topic-arn "arn:aws:sns:ap-south-1:149815208654:trivy_scan_mail" --subject "Trivy Report" --message "Trivy report is available at view-source:https://trivy-scan-bucket.s3.ap-south-1.amazonaws.com/$report_filename View Trivy Scan Report."
 aws sns publish --topic-arn "arn:aws:sns:ap-south-1:149815208654:trivy_scan_mail" \
   --subject "Trivy Report" \
-  --message "Trivy report is available at view-source:https://trivy-scan-bucket.s3.ap-south-1.amazonaws.com/$report_filename'>View Trivy Scan Report."
+  --message "Trivy report is available at view-source:https://trivy-scan-bucket.s3.ap-south-1.amazonaws.com/$report_filename View Trivy Scan Report."
